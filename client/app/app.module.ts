8441896bd43b444caee25990deeb289290dfc0a7
Dimitar Kalaydzhiev <dimitar.kalaydzhievv@gmail.com>
2016-12-24 16:19:32 +0200

    Added app routing and home module

--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -1,9 +1,16 @@
+/* App modules */
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Router } from '@angular/router';
 
+/* Custom Modules */
+import { HomeModule } from './home/home.module';
+
+/* Components */
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
@@ -12,9 +19,15 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    HomeModule,
+    RouterModule.forRoot([
+      { path: '', redirectTo: '/', pathMatch: 'full' },
+      { path: '**', redirectTo: '/', pathMatch: 'full' }
+    ], { useHash: true }),
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
