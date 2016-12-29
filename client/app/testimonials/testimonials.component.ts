edaa3d92b6d82389b01f41d7a7044db9de960fd9
Azonic89 <ooze89@yahoo.com>
2016-12-29 09:48:14 +0200

    Added Spinner Service for Testimonials page

--- client/app/testimonials/testimonials.component.ts
+++ client/app/testimonials/testimonials.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SpinnerService } from '../shared/services';
 
 @Component({
   selector: 'app-testimonials',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TestimonialsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private spinnerService:SpinnerService) { }
 
   ngOnInit() {
+    this.spinnerService.show();
+  }
+
+  public ngAfterViewInit(): void {
+    this.spinnerService.hide(1000);
   }
 
 }
