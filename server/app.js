3c98d4e1523237e397921cf23cdea21adda8d489
Azonic89 <ooze89@yahoo.com>
2016-12-29 09:14:38 +0200

    fixes in about, server, initial design

--- server/app.js
+++ server/app.js
@@ -21,15 +21,30 @@ require('./router')(app, data);
 // };
 // data.createUser(user);
 let about = {
-    content: 'admin',
-    team: [{
-        avatar: '/asdads/123',
-        name: 'pesho',
-        jobTitle: 'shef',
-        socialMediaIcons: []
-    }, ],
+    content: 'We are young and tough and we are trying to rock the world with this app. Unforunately we r using Angular 2 ><',
+    avatar: 'https://scontent-frt3-1.xx.fbcdn.net/v/t1.0-9/14702243_1455497067799478_955787893861110435_n.jpg?oh=672068d2a17145e80da2812619bbc08a&oe=5922CECB',
+    name: 'Vasil Penev',
+    jobTitle: 'CEO',
+    socialMediaIcons: []
 };
 data.createAboutData(about);
 
-app.start();
+let about2 = {
+    content: 'We are young and tough and we are trying to rock the world with this app. Unforunately we r using Angular 2 ><',
+    avatar: 'https://scontent-frt3-1.xx.fbcdn.net/v/t1.0-9/425116_235117463238050_1408560973_n.jpg?oh=f0e09072f195b7936a1a7abcd89a930c&oe=58E19476',
+    name: 'Dimitur Kalaydzhiev',
+    jobTitle: 'CEO',
+    socialMediaIcons: []
+};
+data.createAboutData(about2);
 
+let about3 = {
+    content: 'We are young and tough and we are trying to rock the world with this app. Unforunately we r using Angular 2 ><',
+    avatar: 'https://scontent-frt3-1.xx.fbcdn.net/v/t1.0-9/13906970_10210535840300100_2497753137668459306_n.jpg?oh=e9faadf031de7ee3bb96199c11a26af8&oe=58EE0C6F',
+    name: 'Zorka Shindova',
+    jobTitle: 'CEO',
+    socialMediaIcons: []
+};
+data.createAboutData(about3);
+
+app.start();
