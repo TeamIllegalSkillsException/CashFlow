6a15c602e67e1cd569c3fc14f237b26473c0a373
zorie <shindova@abv.bg>
2017-01-03 00:27:59 +0200

    Updating bill functionality done to some point

--- client/app/dashboard/services/bills.services.ts
+++ client/app/dashboard/services/bills.services.ts
@@ -10,6 +10,8 @@ export class BillsService {
     private userBillsUrl: string = '/api/user-bills';
     private billCategoriesUrl: string = '/api/bill-categories';
     private billRecurrencesUrl: string = '/api/bill-recurrences';
+    private updateBillUrl: string = '/api/update-bill';
+    
     
 
     constructor(private http: Http, private userService: UserService,
@@ -48,5 +50,14 @@ export class BillsService {
                                             .createHttpRequesterOptions(this.billRecurrencesUrl, {}, authHeaderObject);
 
         return this.httpRequesterService.get(httpRequestOptions);
+    }
+
+    updateBill(bill): Observable<Response> {
+        let authHeaderObject = this.userService.getAuthHeaderObject();
+
+        const httpRequestOptions = this.httpRequesterOptionsFactory
+                .createHttpRequesterOptions(this.userBillsUrl, bill, authHeaderObject);
+
+        return this.httpRequesterService.post(httpRequestOptions);
     }
 };
